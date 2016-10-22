2f20c8efb9e10a031905b63414bd57fdb2df7316
Cosmin Roman <coosmiin@yahoo.com>
2016-10-22 22:31:21 +0300

    Added first two projects: the console and the proxy; Tried the Titanium Web Proxy..

---
+++ src/StretchOS.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using StretchOS.Proxy;
+
+namespace StretchOS.Console
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var osProxy = new OsProxy();
+
+			osProxy.StartProxy();
+
+			// TODO: the namespace is the same; can we solve this somehow?
+			System.Console.Read();
+
+			// TODO: make sure the proxy settings are removed when the console app is closed
+			// TODO: check that it works together with fiddler
+		}
+	}
+}
---
+++ src/StretchOS.Proxy/OsProxy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Models;
+
+namespace StretchOS.Proxy
+{
+	public class OsProxy
+	{
+		public void StartProxy()
+		{
+			var proxyServer = new ProxyServer();
+			proxyServer.TrustRootCertificate = true;
+
+			proxyServer.BeforeRequest += ProxyServer_BeforeRequest;
+
+			var explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any, 8000, true);
+
+			proxyServer.AddEndPoint(explicitEndPoint);
+
+			proxyServer.Start();
+
+			proxyServer.SetAsSystemHttpProxy(explicitEndPoint);
+			proxyServer.SetAsSystemHttpsProxy(explicitEndPoint);
+		}
+
+		private async Task ProxyServer_BeforeRequest(object sender, SessionEventArgs e)
+		{
+			Console.WriteLine(e.WebSession.Request.Url);
+		}
+	}
+}
