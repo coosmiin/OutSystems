9ab81c696a8c07625493c0a3d0bac5ac75edf818
Cosmin Roman <coosmiin@yahoo.com>
2017-03-28 18:42:53 +0300

    Added project structure for Lifetime and ServiceCenter commands

---
+++ Commands/src/StretchOS.Lifetime/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StretchOS.Lifetime
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello Lifetime!");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Commands/src/StretchOS.ServiceCenter/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StretchOS.ServiceCenter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello ServiceCenter!");
+			Console.ReadLine();
+		}
+	}
+}
