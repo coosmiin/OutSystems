1d3b3669d62a3039799b1eacad1dc357cd1ba6db
Cosmin Roman <coosmiin@yahoo.com>
2017-04-10 20:55:32 +0300

    Added test project; Added first test.

---
+++ Commands/src/StretchOS.Selenium.UnitTests/OSWebDriverTests.cs
@@ -0,0 +1,23 @@
+using OpenQA.Selenium;
+using Moq;
+using Xunit;
+
+namespace StretchOS.Selenium.UnitTests
+{
+	public class OSWebDriverTests
+	{
+		[Fact]
+		public void GoTo_CallsNavigateOnInnerDriver()
+		{
+			var navigateMock = new Mock<INavigation>();
+
+			var seleniumWebDriverStub =
+				Mock.Of<IWebDriver>(m => m.Navigate() == navigateMock.Object);
+
+			var osWebDriver = new OSWebDriver(seleniumWebDriverStub);
+			osWebDriver.GoTo("http://www.google.com");
+
+			navigateMock.Verify(m => m.GoToUrl(It.IsAny<string>()), Times.Once());
+		}
+	}
+}
---
+++ Commands/src/StretchOS.Selenium/OSWebDriver.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace StretchOS.Selenium
+{
+	public class OSWebDriver
+	{
+		private readonly IWebDriver _driver;
+
+		public OSWebDriver(IWebDriver webDriver)
+		{
+			_driver = webDriver;
+		}
+
+		public void GoTo(string url)
+		{
+			_driver.Navigate().GoToUrl(url);
+		}
+	}
+}
